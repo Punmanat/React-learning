de14fc08ae2c51aba8d083a242fc634c109d5b22
Punmanat <60070053@kmitl.ac.th>
2022-05-23 14:59:17 +0700

    Refactor stateless function

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,19 +1,18 @@
 import React from "react";
 import Option from "./Option";
 // Stateless
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
 
-      {props.options.map((o) => (
-        <Option
-          option={o}
-          key={o}
-          handleDeleteOption={props.handleDeleteOption}
-        />
-      ))}
-    </div>
-  );
-};
+    {props.options.map((o) => (
+      <Option
+        option={o}
+        key={o}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
+  </div>
+);
+
 export default Options;
